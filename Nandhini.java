e133bb78bc1f99badcc7247be362d480253733f8
Nandymohan <nandhinipsgc@gmail.com>
2022-01-22 10:11:24 +0530

    Added feature chat implementation

--- Nandhini.java
+++ Nandhini.java
@@ -2,5 +2,10 @@ class Nandhini
 {
 public static void main(String args[])
 {
-System.out.println("Heello wrold!");
+System.out.println("Hello wrold!");
 }}
+
+
+1. Whatsapp development
+
+2. Chatting between friends
